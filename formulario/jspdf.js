3cda8dfb46628559a279276b0bbd4659c21b9240
Lautaro192 <lautatapaz@gmail.com>
2023-05-31 22:27:44 -0300

    31/05/2023 10:27 L

--- formulario/jspdf.js
+++ formulario/jspdf.js
@@ -1,12 +1,38 @@
+/* var inputIds = [document.getElementById('inp-entidad'), document.getElementById('inp-domicilio'), document.getElementById('inp-nombreapellido'), document.getElementById('inp-telefono'), document.getElementById('inp-localidad'), document.getElementById('inp-dni')];
+bgas=[],
+
+inputIds.forEach((el, indx) =>{
+    el.style.backgroundColor = "white";
+    el.addEventListener('change', function(){
+      el.style.backgroundColor = "grey";
+      bgas[indx] = el.Value;
+      console.log(bgas);
+    });
+}); */
+
 async function generarPDF() {
     document.getElementById("btn-pdf").innerHTML = "Descargarndo"
 
     //----------
-    var descarga = document.getElementById("imprimir")
-    var doc = new jsPDF('p', 'pt', 'a4')
+    var descarga = document.getElementById("imprimir");
+    var doc = new jsPDF('p', 'pt', 'letter');
+    var margin=10;
+  
+   
+    doc.html(document.body, {
+        x: margin,
+        y: margin,
+        html2canvas: {
+           // scale: scale,
+        },
+        callback: function(doc){
+            doc.output('dataurlnewwindow', {filename: 'formulario.pdf'});
+        }
+    });
+    
 
     await html2canvas(descarga, {
-          width: 550 
+        width: 550 
     } )
 
     doc.save("Documento.pdf")
